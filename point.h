9dbe4104dd02417c79f01d6e3ab7e2a22e1a756c
Nate Boxer <nboxer@usc.edu>
2024-11-19 21:08:47 -0800

    initial commit

---
+++ point.h
@@ -0,0 +1,70 @@
+#include <sstream>
+#include <string>
+#include <iostream>
+
+#ifndef POINT_H
+#define POINT_H
+
+using namespace std;
+
+// Represents one spline control point.
+struct Point
+{
+  double x, y, z;
+
+  Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
+  Point(double *point)
+  {
+    x = point[0];
+    y = point[1];
+    z = point[2];
+  }
+
+  string toString() const
+  {
+    stringstream ss;
+    ss << "(" << x << ", " << y << ", " << z << ")";
+    return ss.str();
+  }
+
+  Point &print()
+  {
+    cout << toString() << endl;
+    return *this;
+  }
+
+  Point operator+(const Point &rhs)
+  {
+    return Point(x + rhs.x, y + rhs.y, z + rhs.z);
+  }
+
+  Point operator-()
+  {
+    return Point(-x, -y, -z);
+  }
+
+  Point &normalize()
+  {
+    double magnitude = sqrt((x * x) + (y * y) + (z * z));
+    x /= magnitude;
+    y /= magnitude;
+    z /= magnitude;
+    return *this;
+  }
+};
+
+Point crossProduct(const Point &A, const Point &B)
+{
+  double Cx = A.y * B.z - A.z * B.y;
+  double Cy = A.z * B.x - A.x * B.z;
+  double Cz = A.x * B.y - A.y * B.x;
+  return Point(Cx, Cy, Cz);
+}
+
+Point operator*(const float a, const Point &p)
+{
+  return Point(a * p.x, a * p.y, a * p.z);
+}
+
+#endif
